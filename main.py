cd2a3947e79d78d3303c7019dd98e7509ea914e1
Durga <happy.durga2458@gmail.com>
2023-12-03 05:42:06 +0530

    created  login and register pages used - flask_wtf wtf FlaskForms

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '743fe7dfa22cfb0f55042e5c30bb8e83'
 
 posts = [
     {
@@ -21,12 +23,29 @@ posts = [
 ]
 
 @app.route("/")
-def hello():
+def home():
     return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
     return render_template('about.html', title='about')
 
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
 
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash("Logged in!", 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('check your credentials!', 'danger')  
+    return render_template('login.html', title='Login', form=form)
 
